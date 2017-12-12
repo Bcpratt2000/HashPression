4380247a61524222c8af9bbede3495de79c70535
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-12-12 13:00:40 -0700

    working on writing and reading metadata currently

--- Debug/src/controller/main.d
+++ Debug/src/controller/main.d
@@ -1,1 +1,14 @@
-src/controller/main.o: ../src/controller/main.cpp
+src/controller/main.o: ../src/controller/main.cpp \
+ ../src/controller/../model/HashPressionController.h \
+ ../src/controller/../model/Util.h ../src/controller/../model/MetaData.h \
+ ../src/controller/../model/Util.h ../src/controller/../model/MetaData.h
+
+../src/controller/../model/HashPressionController.h:
+
+../src/controller/../model/Util.h:
+
+../src/controller/../model/MetaData.h:
+
+../src/controller/../model/Util.h:
+
+../src/controller/../model/MetaData.h:
--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -1,15 +1,30 @@
 //============================================================================
 // Name        : main.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Ben Pratt
 //============================================================================
 
 #include <iostream>
+
+#include "../model/HashPressionController.h"
+#include "../model/Util.h"
+#include "../model/MetaData.h"
+
 using namespace std;
 
 int main() {
-	cout << "!!!Hello W orld!!!" << endl;
+
+	HashPressionController compressor = HashPressionController();
+	string fileToRead = "src/WallOfText.txt";
+
+	string text = compressor.compress(Util::readFile(fileToRead), 3);
+
+	MetaData data = MetaData(text);
+
+	cout << data.getBlocksize() << endl;
+
+	Util::writeFile("src/out.txt", text);
+
+	cout << text << endl;
+
 	return 0;
 }
